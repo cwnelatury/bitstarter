fb9fd5809cfa9457e9930615ba8b65882941366c
Charles Nelatury <cwnelatury@gmail.com>
2013-07-17 20:33:40 +0000

    new schanges
    OBqq
    
    qssr the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html).toString());
+  response.send('bullshit');
 });
 
 var port = process.env.PORT || 5000;
