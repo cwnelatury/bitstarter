1ffbddd5011d64dcd2f61d1178fe3a071ca6de5f
Charles Nelatury <cwnelatury@gmail.com>
2013-07-17 20:40:38 +0000

    blash

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
+var fs = require('fs');
+
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('bullshit');
+  response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
